2311677a9e21e158084deef7a65d54a050cacba5
Shruti Verma <83389088+shru-ti-verma@users.noreply.github.com>
2021-11-23 15:17:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+// const hero = document.querySelector(".hero");
+// const heroboy =  document.querySelector(".hero-boy");
+// const vilan = document.querySelector(".vilan");
+
+
+
+
+// function jump() {
+//     if (hero.classList !=  "animate") {
+//         hero.classList.add("animate");
+//         vilan.style.animation  = "move is infine linera";
+//     }
+//     settimeout(() => {
+//         hero.classList.remove("animate");
+//     }, 300);
+// }
+
+// document.addEventListener("keydown",(e) =>{
+//     if (e.code == "space"){
+//         jump();
+//     }
+// });
+
+// let isAlive = setInterval(() => {
+//     let heroTop = parseInt(window.getComputedStyle(hero).getPropertyValue("top"));
+//     //   console.log(heroTop);
+//     let vilanLeft = parseInt(
+//       window.getComputedStyle(vilan).getPropertyValue("left")
+//     );
+  
+//     if (vilanLeft < 40 && vilanLeft > 20 && heroTop >= 130) {
+//       vilan.style.animation = "none";
+//       alert("Game over. Press spacebar to start");
+//     }
+//   }, 10);
+
+
+
+const hero = document.querySelector(".hero");
+const heroBoy = document.querySelector(".hero-boy");
+const vilan = document.querySelector(".vilan");
+
+const jump = () => {
+  if (hero.classList != "animate") {
+    hero.classList.add("animate");
+    vilan.style.animation = "move 1s infinite linear";
+  }
+  setTimeout(() => {
+    hero.classList.remove("animate");
+  }, 300);
+};
+
+document.addEventListener("keydown", (e) => {
+  if (e.code == "Space") {
+    jump();
+  }
+});
+
+let isAlive = setInterval(() => {
+  let heroTop = parseInt(window.getComputedStyle(hero).getPropertyValue("top"));
+  //   console.log(heroTop);
+  let vilanLeft = parseInt(
+    window.getComputedStyle(vilan).getPropertyValue("left")
+  );
+
+  if (vilanLeft < 40 && vilanLeft > 20 && heroTop >= 130) {
+    vilan.style.animation = "none";
+    alert("Game over. Press spacebar to start");
+  }
+}, 10);
